6eb5648bf638b348ffbc930f23abb4e9b4ab68d4
Garth Gaughan <garth0323@gmail.com>
2015-01-08 16:26:40 -0700

    inbox styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
+    @messages = Message.where(receiver_id: current_user.id).order("created_at DESC")
   end
 
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #clients
-User.create(first_name: 'Garth', last_name: 'Gaughan', phone: '987654321', email: 'garth@tm.com', type: 'Client', password: '12345678')
-User.create(first_name: 'Charlie', last_name: 'Sampson', phone: '123654321', email: 'charlie@tm.com', type: 'Client', password: '12345678')
+Client.create(first_name: 'Garth', last_name: 'Gaughan', phone: '987654321', email: 'garth@tm.com', password: '12345678')
+Client.create(first_name: 'Charlie', last_name: 'Sampson', phone: '123654321', email: 'charlie@tm.com', password: '12345678')
 
 
 #providers
-User.create(first_name: 'Teresa', last_name: 'Greth', phone: '123456789', email: 'teresa@tm.com', type: 'Provider', password: '12345678')
-User.create(first_name: 'Regina', last_name: 'Gutierrez', phone: '142537891', email: 'regina@tm.com', type: 'Provider', password: '12345678')
+Provider.create(first_name: 'Teresa', last_name: 'Greth', phone: '123456789', email: 'teresa@tm.com', password: '12345678')
+Provider.create(first_name: 'Regina', last_name: 'Gutierrez', phone: '142537891', email: 'regina@tm.com', password: '12345678')
+
+#messages
+Message.create(subject: 'Client 1 Subject', body: 'Test 1 Body', sender_id: Client.first.id, receiver_id: Provider.first.id)
+Message.create(subject: 'Client 2 Subject', body: 'Test 2 Body', sender_id: Client.first.id, receiver_id: Provider.last.id)
+Message.create(subject: 'Client 3 Subject', body: 'Test 3 Body', sender_id: Client.last.id, receiver_id: Provider.first.id)
+Message.create(subject: 'Client 4 Subject', body: 'Test 4 Body', sender_id: Client.last.id, receiver_id: Provider.last.id)
+
+Message.create(subject: 'Provider 1 Subject', body: 'Test 1 Body', sender_id: Provider.first.id, receiver_id: Client.first.id)
+Message.create(subject: 'Provider 2 Subject', body: 'Test 2 Body', sender_id: Provider.first.id, receiver_id: Client.last.id)
+Message.create(subject: 'Provider 3 Subject', body: 'Test 3 Body', sender_id: Provider.last.id, receiver_id: Client.first.id)
+Message.create(subject: 'Provider 4 Subject', body: 'Test 4 Body', sender_id: Provider.last.id, receiver_id: Client.last.id)
