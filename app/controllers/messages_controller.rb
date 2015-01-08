144d6cbf7486ff8235a445cdc06d6b6caff72b29
Garth Gaughan <garth0323@gmail.com>
2015-01-08 14:35:33 -0700

    create message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,4 +5,20 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    binding.pry
+    @message = Message.create(message_params)
+    if @message.save
+      redirect_to messages_path, notice: 'Message was successfully sent!'
+    else
+      redirect_to messages_path, notice: 'There was an error sending your message!'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit!
+  end
+
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,8 @@
 class Provider < User
   has_many :messages
 
+  def name_and_email
+    "#{first_name} #{last_name}, #{email}"
+  end
+
 end
