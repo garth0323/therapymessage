88a3dc6ef026ae8417ab9c8bef35d8e0f70208ba
Garth <garth0323@gmail.com>
2015-01-08 22:38:00 -0700

    change compose

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,17 @@ class MessagesController < ApplicationController
     @sent = Message.where(sender_id: current_user.id).order("created_at DESC")
   end
 
+  def show
+    @message = Message.where(id: params[:id]).first
+  end
+
   def create
-    binding.pry
-    @message = Message.create(message_params)
+    if current_user == "Provider"
+      receiver_id = User.where(email: params[:receiver_id]).first.receiver_id
+      @message = Message.create(message_params).merge(receiver_id: receiver_id)
+    else
+      @message = Message.create(message_params)
+    end
     if @message.save
       redirect_to messages_path, notice: 'Message was successfully sent!'
     else
